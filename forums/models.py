1f1c7b252b780bf9c34d786f096f05409c135f48
rhapidfyre <maharris84@hotmail.com>
2020-05-08 15:21:36 -0700

    initial models setup
    initial models setup

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+  author  = models.OneToOneField(User,on_delete=models.CASCADE)
+  title   = models.CharField(max_length=128)
+  created = models.DateTimeField(auto_now=False,auto_now_add=True)
+  edited  = models.DateTimeField(auto_now=True)
+  hidden  = models.BooleanField(default=False) #If only admins should see it 
+  message = models.TextField()
+  
+class Comments(models.Model):
+  author  = models.OneToOneField(User,on_delete=models.CASCADE)
+  topic   = models.OneToOneField(Post,on_delete=models.CASCADE)
+  title   = models.CharField(max_length=128)
+  created = models.DateTimeField(auto_now=False,auto_now_add=True)
+  edited  = models.DateTimeField(auto_now=True)
+  hidden  = models.BooleanField(default=False) # AKA: Soft-Delete
