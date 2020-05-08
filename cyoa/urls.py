ea05a000c735f7f02d4808e477ce2aa7fee106c7
rhapidfyre <maharris84@hotmail.com>
2020-05-08 14:00:49 -0700

    Finished URLS.py setup

--- cyoa/urls.py
+++ cyoa/urls.py
@@ -17,25 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from creator import urls as creatorUrls
 from dashboard import urls as dashboardUrls
+from forum import urls as forumsUrls
 
 urlpatterns = [
+    path('', include(dashboardUrls)),
+    path('forums/', include(forumsUrls)),
     path('admin/', admin.site.urls),
     path('create/adv/', include(creatorUrls)),
-    path('', include(dashboardUrls)),
-    path('', views.displayNewAdventure),
-    path('<int:adventureID>/add', views.displayNewSlide),
-    path('<int:adventureID>/add/<int:priorContainerID>/add/<int:fromChoiceID>/assign', views.displayNewSlide),
-    path('<int:adventureID>/add/<int:priorContainerID>/add/<int:fromChoiceID>/link', views.displayLinkSlide),
-    path('<int:adventureID>/add/<int:containerID>/add', views.displayNewChoice),
-    path('<int:adventureID>/edit/<int:containerID>/edit', views.displayEditSlide),
-    path('<int:adventureID>/edit/<int:containerID>/delete', views.deleteSlide),
-    path('<int:adventureID>/edit/<int:containerID>/edit/<int:choiceID>', views.displayEditChoice),
-    path('<int:adventureID>/edit/<int:priorContainerID>/edit/<int:fromChoiceID>/assign', views.displayEditNextSlide),
-    path('<int:adventureID>/edit/<int:containerID>/edit/<int:choiceID>/delete', views.deleteChoice),
-    path('<int:adventureID>/submit', views.submitAdventure),
-    path('', views.viewDashboard, name="Dashboard"),
-    path('login/', views.displayLogin, name="Login"),
-    path('login/<int:msgCode>', views.displayLogin, name="Login With Warning"),
-    path("newAccount/", views.displayNewAccount, name="New Account"),
-    path("logout/", views.logout, name="logout"),
+    
 ]
