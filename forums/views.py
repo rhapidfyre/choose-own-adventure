d4451f0bd39a9383c7e3fdc84ea91c3227a852cd
rhapidfyre <maharris84@hotmail.com>
2020-05-08 17:14:39 -0700

    asdasdas
    sdasdasda

--- forums/views.py
+++ forums/views.py
@@ -7,4 +7,4 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def displayForums(request):
-  return render(request, "login/", context="")
+  return render(request, "forums/index.html", context={})
